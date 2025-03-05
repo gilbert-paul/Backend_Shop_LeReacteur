e04c2b6ec90f8dd43f39d1228a37e106019bc503
Paul <paul.gilbert.1996@gmail.com>
2025-03-05 11:08:14 +0100

    add env variables

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const dotenv = require('dotenv')
 
 const userRoutes = require("./routes/user.js");
 const productRoutes = require("./routes/product.js");
 const orderRoutes = require("./routes/order.js");
 
+
+
 const app = express();
 app.use(express.json());
-
+dotenv.config()
+const PORT = process.env.PORT;
 app.use(cors())
 
 app.use(userRoutes);
 app.use(productRoutes);
 app.use(orderRoutes);
 
-mongoose.connect("mongodb://localhost:27017/shopjsv2");
+mongoose.connect(process.env.MONGODB_URI || '');
 
 app.all("*", (req, res) => {
   res.status(404).json({ message: "This route does not exist" });
 });
 
-app.listen(3000, () => console.log("Server started"));
+app.listen(PORT, () => console.log("Server started"));
